971d22714962101fe2e7cf88fd57486b5c44b084
nataliel2514 <natalielopez2514@gmail.com>
2024-01-15 16:06:43 -0600

    setup express server

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('node:http');
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3000;
+const home = fs.readFileSync('index.html');
+
+const server = http.createServer((req, res) => {
+    url = req.url;
+    res.setHeader('Content-Type', 'text/html');
+    if(url == '/'){
+        res.end(home);
+    } else {
+        res.statusCode = 404;
+        res.end("<h1>404 not found</h1>");
+    }
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
